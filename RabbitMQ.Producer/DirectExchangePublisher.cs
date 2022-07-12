5b46f5a7a450b50a909849938a4a65a7c8952e74
Mehmet Selim Sarıyıldız <mehmetselim.sariyildiz@wy.com.tr>
2022-07-13 02:26:55 +0300

    Add project files.

---
+++ RabbitMQ.Producer/DirectExchangePublisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    public static class DirectExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                { "x-message-ttl", 3000 }
+            };
+            channel.ExchangeDeclare("demo-direct-exchange", ExchangeType.Direct, arguments: ttl);
+            var count = 0;
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("demo-direct-exchange", "account.init", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Producer
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { Uri = new Uri("amqp://guest:13579630@localhost:5672") };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            DirectExchangePublisher.Publish(channel);
+
+        }
+    }
+}
